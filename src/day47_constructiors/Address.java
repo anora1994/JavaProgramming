8c82ae5e9e1d6031e0ddf299794d0c887b1436de
anora1994 <anoragranat@gmail.com>
2021-06-11 13:27:36 -0400

    added Address class

---
+++ src/day47_constructiors/Address.java
@@ -0,0 +1,61 @@
+package day47_constructiors;
+
+public class Address {
+   private String adress;
+    private String city;
+    private String state;
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "adress='" + adress + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", zipCode='" + zipCode + '\'' +
+                ", country='" + country + '\'' +
+                '}';
+    }
+
+    private String zipCode;
+    private String country = "USA";
+}
---
+++ src/day47_constructiors/myAddress.java
@@ -0,0 +1,9 @@
+package day47_constructiors;
+
+public class myAddress {
+    public static void main(String[] args) {
+        Address myAddr = new Address();
+        myAddr.setAdress("Java 123 St");
+
+    }
+}
