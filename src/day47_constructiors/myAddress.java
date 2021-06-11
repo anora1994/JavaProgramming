933642991d1d33e3710222af8cef416134710d61
anora1994 <anoragranat@gmail.com>
2021-06-11 13:32:34 -0400

    added more info to MyAddress class

--- src/day47_constructiors/myAddress.java
+++ src/day47_constructiors/myAddress.java
@@ -4,6 +4,11 @@ public class myAddress {
     public static void main(String[] args) {
         Address myAddr = new Address();
         myAddr.setAdress("Java 123 St");
+        myAddr.setCity("McClean");
+        myAddr.setState("VA");
+        myAddr.setZipCode("22182");
+
+        System.out.println("myAddr = " + myAddr);
 
     }
 }
